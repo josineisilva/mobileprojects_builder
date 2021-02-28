6e226b955cc1d4f0c3dcd08188ca6eb6502790ae
josineisilva <josinei.lopessilva@gmail.com>
2021-02-28 00:54:08 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Builder"),
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text('SnackBar'),
+          ),
+        );
+      }),
+    );
+  }
+}
